44e0f9a03260ab612b2d82995d0671d59c28d045
raoaershu09 <125963806+raoaershu09@users.noreply.github.com>
2024-12-26 13:05:47 +0500

    drawer completed

--- lib/screens/user-panel/main-screen.dart
+++ lib/screens/user-panel/main-screen.dart
@@ -1,13 +1,11 @@
-import 'package:firebase_auth/firebase_auth.dart';
+// ignore: file_names
+// ignore_for_file: file_names, duplicate_ignore
+
 import 'package:flutter/material.dart';
 
-import 'package:get/get.dart';
-
-import 'package:google_sign_in/google_sign_in.dart';
-
-import 'package:laptopharbor/screens/auth-ui/welcome-screen.dart';
-
 import 'package:laptopharbor/utils/app-constant.dart';
+
+import 'package:laptopharbor/widgets/custom-drawer-widget.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -16,27 +14,14 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
         backgroundColor: AppConstant.appMainColor,
-        title: Text(AppConstant.appMainName),
+        title: Text(AppConstant.appMainName,
+        style: TextStyle(color: AppConstant.appTextColor),
+        ),
         centerTitle: true,
-
-        actions: [
-          GestureDetector(
-            onTap: () async {
-            GoogleSignIn googleSignIn = GoogleSignIn();
-            FirebaseAuth _auth = FirebaseAuth.instance;
-            await _auth.signOut();
-            await googleSignIn.signOut();
-            Get.offAll(() => WelcomeScreen());
-            },
-
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(Icons.logout),
-              ),
-          )
-        ],
       ),
+      drawer: DrawerWidget(),
     );
   }
 }
---
+++ lib/widgets/custom-drawer-widget.dart
@@ -0,0 +1,153 @@
+// ignore_for_file: file_names, sort_child_properties_last, no_leading_underscores_for_local_identifiers
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import 'package:google_sign_in/google_sign_in.dart';
+
+import 'package:laptopharbor/screens/auth-ui/welcome-screen.dart';
+
+import 'package:laptopharbor/utils/app-constant.dart';
+
+class DrawerWidget extends StatefulWidget {
+  const DrawerWidget({super.key});
+
+  @override
+  State<DrawerWidget> createState() => _DrawerWidgetState();
+}
+
+class _DrawerWidgetState extends State<DrawerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: Get.height / 25),
+      child: Drawer(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(20.0),
+            bottomRight: Radius.circular(20.0),
+          ),
+        ),
+
+        child: Wrap(
+          runSpacing: 10,
+          children: [
+            Padding(
+              padding:
+              const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+              child: ListTile(
+                titleAlignment: ListTileTitleAlignment.center,
+                title: Text("Aershuman",
+                style: TextStyle(color: Colors.white),
+                ),
+                subtitle: Text("Version 2.0.1",
+                style: TextStyle(color: Colors.white),
+                ),
+                leading: CircleAvatar(
+                  radius: 22.0,
+                  backgroundColor: AppConstant.appMainColor,
+                  child: Text("A",
+                  style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              ),
+              Divider(
+                indent: 10.0,
+                endIndent: 10.0,
+                thickness: 1.5,
+                color: Colors.grey,
+              ),
+
+              Padding(
+              padding:
+              const EdgeInsets.symmetric(horizontal: 20.0,),
+              child: ListTile(
+                titleAlignment: ListTileTitleAlignment.center,
+                title: Text("Home",
+                style: TextStyle(color: Colors.white),
+                ),
+                leading: Icon(Icons.home,
+                  color: Colors.white,),
+                  trailing: Icon(Icons.arrow_forward,
+                  color: Colors.white,),
+              ),
+              ),
+
+                Padding(
+              padding:
+              const EdgeInsets.symmetric(horizontal: 20.0,),
+              child: ListTile(
+                titleAlignment: ListTileTitleAlignment.center,
+                title: Text("Products",
+                style: TextStyle(color: Colors.white),
+                ),
+                leading: Icon(Icons.production_quantity_limits,
+                color: Colors.white,),
+                trailing: Icon(Icons.arrow_forward,
+                color: Colors.white,
+                ),
+              ),
+              ),
+
+                Padding(
+              padding:
+              const EdgeInsets.symmetric(horizontal: 20.0,),
+              child: ListTile(
+                titleAlignment: ListTileTitleAlignment.center,
+                title: Text("Orders",
+                style: TextStyle(color: Colors.white),
+                ),
+                leading: Icon(Icons.shopping_bag_sharp,
+                color: Colors.white,),
+                trailing: Icon(Icons.arrow_forward,
+                color: Colors.white,),
+              ),
+              ),
+
+                Padding(
+              padding:
+              const EdgeInsets.symmetric(horizontal: 20.0,),
+              child: ListTile(
+                titleAlignment: ListTileTitleAlignment.center,
+                title: Text("Contact",
+                style: TextStyle(color: Colors.white),
+                ),
+                leading: Icon(Icons.phone_android,
+                color: Colors.white,),
+                trailing: Icon(Icons.arrow_forward,
+                color: Colors.white,),
+              ),
+              ),
+
+              Padding(
+              padding:
+              const EdgeInsets.symmetric(horizontal: 20.0,),
+              child: ListTile(
+                onTap: () async{
+                  GoogleSignIn googleSignIn = GoogleSignIn();
+                  FirebaseAuth _auth = FirebaseAuth.instance;
+                  await _auth.signOut();
+                  await googleSignIn.signOut();
+                  Get.offAll(() => WelcomeScreen());
+                },
+                titleAlignment: ListTileTitleAlignment.center,
+                title: Text("Logout",
+                style: TextStyle(color: Colors.white),
+                ),
+                leading: Icon(Icons.logout,
+                color: Colors.white,),
+                trailing: Icon(Icons.arrow_forward,
+                color: Colors.white,),
+              ),
+              ),
+          ],
+        ),
+        backgroundColor: AppConstant.appSecondoryColor,
+      ),
+    );
+  }
+}
